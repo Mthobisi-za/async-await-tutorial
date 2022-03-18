c3806924e3c5db80eb30d7a715edeeb314df49bf
Mthobisi-za <mthobisimtho333@gmail.com>
2022-03-18 10:57:05 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,117 @@
+const data = [{
+        "gender": "female",
+        "name": {
+            "title": "Ms",
+            "first": "Abigail",
+            "last": "Shelton"
+        },
+        "location": {
+            "street": {
+                "number": 6931,
+                "name": "Richmond Park"
+            },
+            "city": "Edenderry",
+            "state": "Cork",
+            "country": "Ireland",
+            "postcode": 17948,
+            "coordinates": {
+                "latitude": "18.8773",
+                "longitude": "37.3089"
+            },
+            "timezone": {
+                "offset": "-3:30",
+                "description": "Newfoundland"
+            }
+        },
+        "email": "abigail.shelton@example.com",
+        "login": {
+            "uuid": "0dd4ba62-19b4-4668-8bb9-b0c40e0c6145",
+            "username": "organicpeacock137",
+            "password": "qqqqqqqq",
+            "salt": "JRpyyHHU",
+            "md5": "b2f6cec1a333dfdb27fe837e3c76f584",
+            "sha1": "67f90d37a8f585013a5a7636a0ada3f311e71605",
+            "sha256": "f3ec767083635b917a4b1b870a46766f15859030a2c9f3d918a515f1466c86ca"
+        },
+        "dob": {
+            "date": "1987-08-16T21:51:19.719Z",
+            "age": 35
+        },
+        "registered": {
+            "date": "2007-08-11T17:05:19.627Z",
+            "age": 15
+        },
+        "phone": "071-074-0235",
+        "cell": "081-480-5307",
+        "id": {
+            "name": "PPS",
+            "value": "1272559T"
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/5.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/5.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/5.jpg"
+        },
+        "nat": "IE"
+    },
+
+
+    {
+        "gender": "male",
+        "name": {
+            "title": "Mr",
+            "first": "Josef",
+            "last": "Ward"
+        },
+        "location": {
+            "street": {
+                "number": 8173,
+                "name": "The Green"
+            },
+            "city": "Oxford",
+            "state": "East Sussex",
+            "country": "United Kingdom",
+            "postcode": "VA3 7ZN",
+            "coordinates": {
+                "latitude": "6.0732",
+                "longitude": "-39.8582"
+            },
+            "timezone": {
+                "offset": "-1:00",
+                "description": "Azores, Cape Verde Islands"
+            }
+        },
+        "email": "josef.ward@example.com",
+        "login": {
+            "uuid": "8e6dea52-c52f-4527-ac6b-78c6f54669d8",
+            "username": "happypeacock536",
+            "password": "ronald1",
+            "salt": "Wxi8tkE4",
+            "md5": "077aee3b3332c00cb7b7d2d387389c28",
+            "sha1": "5567ff8684ef2682fcccd4002d0f5c30775385db",
+            "sha256": "f9a7a87990a0604cfe7e5fc4d3de5772b83064846ace86c1cbf45994d69747ee"
+        },
+        "dob": {
+            "date": "1974-09-05T06:19:39.395Z",
+            "age": 48
+        },
+        "registered": {
+            "date": "2015-06-08T02:05:47.358Z",
+            "age": 7
+        },
+        "phone": "016974 47121",
+        "cell": "0707-571-922",
+        "id": {
+            "name": "NINO",
+            "value": "PG 75 45 74 N"
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/40.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/40.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/40.jpg"
+        },
+        "nat": "GB"
+    }
+
+
+]
